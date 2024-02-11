0d3a58d424ae7117dc99d22012a01593e42bbbdb
Dalton Tewanger <daltontewanger@yahoo.com>
2024-02-11 13:53:08 -0800

    Added basic game screen set up with buttons and instructions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+// Allows you to select the instructions to display them over the gameContainer //
+function showInstructions() {
+  document.getElementById("gameTitle").style.display = "none";
+  document.getElementById("playButton").style.display = "none";
+  document.getElementById("instructionsButton").style.display = "none";
+  document.getElementById("instructionsContainer").style.display = "block";
+}
+
+// Returns you to the main screen //
+function hideInstructions() {
+  document.getElementById("gameTitle").style.display = "block";
+  document.getElementById("playButton").style.display = "block";
+  document.getElementById("instructionsButton").style.display = "block";
+  document.getElementById("instructionsContainer").style.display = "none";
+}
